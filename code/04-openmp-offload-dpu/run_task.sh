f63c6d64d4b7b82a8c87b2e664bdfe3d3925574e
Mariano Benito <mbenitohoz@gmail.com>
2024-11-15 14:00:20 +0100

    Include run_task and run_service for automatic mode

---
+++ code/04-openmp-offload-dpu/run_task.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+#SBATCH --reservation sc24_tutorial
+#SBATCH --partition rg-nextgen-hpc
+#SBATCH --nodelist dash3-bf3-1
+#SBATCH --output task-%j.out
+#SBATCH --error task-%j.err
+#SBATCH --time 0:1:0
+
+# the following line is for task c
+if [ -n "$LIB_" ]; then
+	echo "lib path: $LIB_"
+	export LD_LIBRARY_PATH=$LIB_/x86:$LD_LIBRARY_PATH
+fi
+
+# Loading modules
+module use /projects/tools/x86_64/ubuntu-22.04/modulefiles/
+module load odos-dpu
+
+COMMAND_=$@
+
+echo "cmd : $COMMAND_"
+
+$COMMAND_
